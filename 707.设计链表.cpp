9be3ae190b37757b4f101a3179314d6eada14ccc
贾斌 <raindropsaber@gmail.com>
2019-08-13 22:34:48 +0800

    Update 707.设计链表.cpp

--- 707.设计链表.cpp
+++ 707.设计链表.cpp
@@ -1,9 +1,12 @@
 class MyLinkedList {
 public:
     struct Node{
-    int val;
-    Node* next;
-    Node(int x):val(x),next(NULL){}
+        int val;
+        Node* next;
+        Node(int x){
+            val=x;
+            next=NULL;
+        }
     };
     int len;
     Node *head,*tail;//head指向头结点，tail指向尾节点
@@ -109,4 +112,3 @@ public:
         }
     }
 };
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
